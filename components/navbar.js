bf0ce837782afe349f6bf8f03810524d5cda2e65
Andy Chang <andychang268@gmail.com>
2023-05-08 23:42:27 -0700

    Navbar done, see attached diagram for implementation. Need to decouple font from the specific .js file for reuse.

--- components/navbar.js
+++ components/navbar.js
@@ -1,23 +1,32 @@
 import styles from '../styles/navbar-styles.module.css';
 import Link from 'next/link';
+import { Fira_Sans } from 'next/font/google';
+
+//import fira sans font, then we can pass it down so css knows.
+export const fira = Fira_Sans({
+  subsets: ['latin'],
+  weight: ['400','700','900'],
+  style: ['italic', 'normal']
+});
+
 
 export default function Navbar() {
   return (
-    <div id={styles.Navbar}>
+    <div id={styles.Navbar}> {/*this will be flexxed */}
+      <div id={styles.title} className={fira.className}>Quick-R-Build</div>
 
-      <div id={styles.title}> Quick-R-Build</div>
       <div id={styles.links}>
         <ul className={styles.navUl}>
-          <Link href="/" className={styles.link} >Resume/CV </Link>
-          <Link href="/" className={styles.link}>Job Description</Link>
-          <Link href="/" className={styles.link}>Result</Link>
+          <Link href="/" className={`${fira.className} ${styles.link}`}>Resume/CV</Link>
+          <Link href="/" className={`${fira.className} ${styles.link}`}>Job Description</Link>
+          <Link href="/" className={`${fira.className} ${styles.link}`}>Result</Link>
         </ul>
         
         <div className={styles.divider}></div>
 
         <ul className={styles.navUl}>
-          <Link href="/" className={styles.link}>About</Link> 
-          <Link href="/" className={styles.link}>Attributions</Link>
+          <Link href="/" className={`${fira.className} ${styles.link}`}>About</Link> 
+          <Link href="/" className={`${fira.className} ${styles.link}`}>Attributions</Link>
         </ul>
       </div>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import '../styles/global.css';
 
-// Top Level App to store Global CSS
-
 export default function App({ Component, pageProps}) {
   return <Component {...pageProps} />
 }
