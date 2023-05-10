fb20b1b2bc87630267dc340033127dd565d037ad
DHSYan <sean.yan@uwaterloo.ca>
2023-05-10 18:24:44 +0800

    make the jobdescription variable global

--- pages/api/form-resume.js
+++ pages/api/form-resume.js
@@ -1,9 +1,11 @@
+export var jobDescription;
+
 export default function handler(req, res){
 
     const body = req.body
 
     // res.status(200).json(`{name: ${body.jobDescription}}`)
-    var jobDescription = body.jobDescription;
+    jobDescription = body.jobDescription;
     if (req.method === "POST") {
         console.log("We got a POST");
         // res.status(200).json(`{name: ${body.jobDescription}}`);
