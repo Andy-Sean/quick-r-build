620c10b069acc7efddae3ab4fd94ac8f3292a78f
Andy Chang <andychang268@gmail.com>
2023-05-10 19:01:06 -0700

    refactored navbar, courtesy of Hans Sun :)

--- components/navbar.js
+++ components/navbar.js
@@ -2,32 +2,28 @@ import styles from '../styles/navbar-styles.module.css';
 import Link from 'next/link';
 import { fira } from '../utils/fonts'; //font input
 
-export default function Navbar( { openPage }) {
+//Refactoring idea courtesy of Hans Sun the almighty
+function HighlightableLink( { highlight, href, display }) {
+  if (highlight) { return <Link href={href} className={`${styles.onPage} ${fira.className} ${styles.link}`}>{display}</Link> }
+  else { return <Link href={href} className={`${fira.className} ${styles.link}`}>{display}</Link> }
 
-  //this jank allows for link to be highlighted - a bit janky but it works so...?
-  const pageNames = [ ["Quick-R-Build","/"], ["Resume/CV","/input-resume"], ["Job Description","/job-desc"], ["Result","/result"], ["About","/about"], ["Attributions","/attributions"] ];
-  const linkify = pageNames.map(
-    (str)=> {if (openPage && openPage === str[0]) 
-              { return <Link href={str[1]} key={str[0]} className={`${styles.onPage} ${fira.className} ${styles.link}`}>{str[0]}</Link> }
-            else 
-              {return <Link href={str[1]} key={str[0]} className={`${fira.className} ${styles.link}`}>{str[0]}</Link>}}
-  );
-
-  const leftLinks = linkify.slice(1,4);
-  const rightLinks = linkify.slice(4,6);
-
+}
+export default function Navbar( { openPage }) { 
   return (
     <div id={styles.Navbar}> {/*this will be flexxed */}
-      <div id={styles.title}>{linkify[0]}</div>
+      <div id={styles.title}><HighlightableLink highlight={openPage==="Quick-R-Build"} href="/" display="Quick-R-Build" /></div>
       <div id={styles.links}>
         <ul className={styles.navUl}>
-          {leftLinks}
+          <HighlightableLink highlight={openPage==="Resume/CV"} href="/input-resume" display="Resume/CV" />
+          <HighlightableLink highlight={openPage==="Job Description"} href="/job-desc" display="Job Description" />
+          <HighlightableLink highlight={openPage==="Result"} href="/result" display="Result" />
         </ul>
         
         <div className={styles.divider}></div>
 
         <ul className={styles.navUl}>
-          {rightLinks}
+          <HighlightableLink highlight={openPage==="About"} href="/about" display="About" />
+          <HighlightableLink highlight={openPage==="Attributions"} href="/attributions" display="Attributions" />
         </ul>
       </div>
     </div>
