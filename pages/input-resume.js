c63fbdacfc32c3661ddaac4cc5ae5d7b25f23e04
Andy Chang <andychang268@gmail.com>
2023-05-18 21:49:40 -0700

    in progress: dynamic resume display
    it took way too long, but a way to display the resume's contents is
    underway. Might want this on a seperate file, idk. Its very messy.

--- pages/input-resume.js
+++ pages/input-resume.js
@@ -1,16 +1,19 @@
 import Navbar from '../components/navbar';
+import Link from 'next/link';
 
-function JobDescription(){
+
+function Sidebar() { //options user has access to
   return (
-    <>
-      <form action="/api/form-resume" method="post">
-        <label for="jobDescription">Job Description: </label>
-        <input name="jobDescription" type="text"/>
-        <button type="submit"> Submit! </button>
-      </form>
-    </>
-  )  
+    <nav>
+      <ul>
+        <li><img/><Link href="#">New Entry</Link></li>
+        <li><img/><Link href="#">Import from JSON</Link></li>
+        <li><img/><Link href="#">Export to JSON</Link></li>
+      </ul>
+    </nav>
+  )
 }
+
 
 function ResumeContent(){
     return (
@@ -32,12 +35,47 @@ function ResumeContent(){
     )
 }
 
+// function ResumeItemDisplay(resumeObject) { 
+//   let output = [];
+//   for (const [category,entries] in Object.entries(resumeObject)){
+//     output.push(<div key={category}>
+//       {entries.map( (entry) => { <p>{entry}</p>})}
+//     </div>)
+//   }
+//   return output;
+// }
+
+//this is what the json object is HOPEFULLY gonna look like at the end...
+let sampleResume = { //replace each thing in the array with an object eventually
+  "Education": [ "Waterloo", "Laurier", "High School" ],
+  "Skills": [ "React", "JS", "Racket" ],
+  "Experience": [],
+  "Projects": [ "Single Trig Expression Reducer", "Quick-R-Build", "Death"],
+}
+
+function RenderResume( { resumeObject }) {
+  return (
+    <ul>
+    {Object.entries(resumeObject).map(([k,v]) => { console.log(v);
+      return <li key={k}>Category: {k}
+        <ul>
+          {v.map( (val,ind) => {console.log(k+ind); return <li key={k+ind}>{val}</li> })}
+        </ul>
+      </li>}
+      )}
+     </ul>
+  )
+}
+
+
 export default function Main(){
   return (
       <>
         <Navbar openPage="Resume/CV"/>
-        <JobDescription/>
-        <ResumeContent/>
+        <Sidebar/>
+        {/*<JobDescription/> moved this to other page 
+        <ResumeContent/> not sure if i need this right now...?*/}
+        <RenderResume resumeObject={sampleResume}/>
       </>
   )  
     
--- pages/job-desc.js
+++ pages/job-desc.js
@@ -1,10 +1,23 @@
 import Navbar from '../components/navbar';
 
+function JobDescription(){
+  return (
+    <>
+      <form action="/api/form-resume" method="post">
+        <label for="jobDescription">Job Description: </label>
+        <input name="jobDescription" type="text"/>
+        <button type="submit"> Submit! </button>
+      </form>
+    </>
+  )  
+}
+
+
 export default function Main(){
   return (
     <div>
       <Navbar openPage="Job Description"/>
-
+      <JobDescription/>
     </div>
   )  
     
