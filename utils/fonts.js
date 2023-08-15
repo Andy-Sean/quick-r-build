9b0d4b09037245feeda2d8734ea738b4c2ac93c5
Andy Chang (Andoiii) <andychang268@gmail.com>
2023-08-15 16:15:27 -0700

    to emphasize its logic only, i deleted all the frontend

--- utils/fonts.js
+++
@@ -1,8 +0,0 @@
-import { Fira_Sans } from 'next/font/google';
-
-//import fira sans font, then we can pass it down so css knows.
-export const fira = Fira_Sans({
-  subsets: ['latin'],
-  weight: ['400','700','900'],
-  style: ['italic', 'normal']
-});
